34243e9016894a7e1f0cd9207987786cc30b4f69
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-01-22 14:21:49 -0500

    docs: remove mention of delegatecall from verifier doc
    - Because we will more likely use a regular call (safer and delegatecall is unnecessary)

--- foundry/src/CallbackVerificationDispatcher.sol
+++ foundry/src/CallbackVerificationDispatcher.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.0;
 /**
  * @title Dispatch callback verification to external contracts
  * @author PropellerHeads Devs
- * @dev Provides the ability to delegate callback verification to external
- *  contracts. This allows dynamically adding new supported protocols
- *  without needing to upgrade any contracts. External contracts will
- *  be called using delegatecall so they can share state with the main
- *  contract if needed.
+ * @dev Provides the ability call external contracts to perform callback
+ *  verification. This allows dynamically adding new supported protocols
+ *  without needing to upgrade any contracts.
  *
  *  Note Verifier contracts need to implement the ICallbackVerifier interface
  */
