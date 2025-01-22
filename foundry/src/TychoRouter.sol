0c05874477e90b659e12ae9ca7ec5ee3d8f03b58
royvardhan <royvardhan.official@gmail.com>
2025-01-22 20:54:36 +0530

    fix: ci

--- foundry/src/TychoRouter.sol
+++ foundry/src/TychoRouter.sol
@@ -6,12 +6,12 @@ import "@permit2/src/interfaces/IAllowanceTransfer.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
 error TychoRouter__WithdrawalFailed();
 error TychoRouter__InvalidReceiver();
 
 contract TychoRouter is AccessControl {
     IAllowanceTransfer public immutable permit2;
+
     using SafeERC20 for IERC20;
 
     //keccak256("NAME_OF_ROLE") : save gas on deployment
@@ -68,9 +68,8 @@ contract TychoRouter is AccessControl {
     {
         // TODO
     }
-    
 
-   /**
+    /**
      * @dev Allows withdrawing any ERC20 funds if funds get stuck in case of a bug.
      */
     function withdraw(IERC20[] memory tokens, address receiver)
@@ -78,36 +77,35 @@ contract TychoRouter is AccessControl {
         onlyRole(FUND_RESCUER_ROLE)
     {
         if (receiver == address(0)) revert TychoRouter__InvalidReceiver();
-        
-   
+
         for (uint256 i = 0; i < tokens.length; i++) {
+            // slither-disable-next-line calls-loop
             uint256 tokenBalance = tokens[i].balanceOf(address(this));
             if (tokenBalance > 0) {
-                tokens[i].safeTransfer(receiver, tokenBalance);
                 emit Withdrawal(address(tokens[i]), tokenBalance);
+                tokens[i].safeTransfer(receiver, tokenBalance);
             }
         }
     }
-    
+
     /**
      * @dev Allows withdrawing any NATIVE funds if funds get stuck in case of a bug.
      * The contract should never hold any NATIVE tokens for security reasons.
      */
-    function withdrawNative(address receiver) 
-        external 
-        onlyRole(FUND_RESCUER_ROLE) 
+    function withdrawNative(address receiver)
+        external
+        onlyRole(FUND_RESCUER_ROLE)
     {
         if (receiver == address(0)) revert TychoRouter__InvalidReceiver();
-        
+
         uint256 amount = address(this).balance;
         if (amount > 0) {
-             // slither-disable-next-line arbitrary-send-eth
+            emit Withdrawal(address(0), amount);
+            // slither-disable-next-line arbitrary-send-eth
             (bool success,) = receiver.call{value: amount}("");
             if (!success) revert TychoRouter__WithdrawalFailed();
-            emit Withdrawal(address(0), amount);
         }
     }
-
 
     /**
      * @dev Allows this contract to receive native token
