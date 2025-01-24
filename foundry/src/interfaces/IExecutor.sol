1ad04bc9e907df040501077967c889b4076e6a62
Diana Carvalho <diana@propellerheads.xyz>
2025-01-24 16:53:40 +0000

    chore: After rebase fixes
    --- don't change below this line ---
    ENG-4033 Took 5 minutes

--- foundry/src/interfaces/IExecutor.sol
+++ foundry/src/interfaces/IExecutor.sol
@@ -8,23 +8,17 @@ pragma abicoder v2;
 interface IExecutor {
     /**
      * @notice Performs a swap on a liquidity pool.
-     * @dev This method can either take the amount of the input token or the amount
-     * of the output token that we would like to swap. If called with the amount of
-     * the  input token, the amount of the output token will be returned, and vice
-     * versa. Whether it is the input or output that is given, is encoded in the data
-     * parameter.
+     * @dev This method takes the amount of the input token and returns the amount of
+     * the output token which has been swapped.
      *
      * Note Part of the informal interface is that the executor supports sending the received
      *  tokens to a receiver address. If the underlying smart contract does not provide this
      *  functionality consider adding an additional transfer in the implementation.
      *
-     *  This function is marked as `payable` to accommodate delegatecalls, which can forward
-     *  a potential `msg.value` to it.
-     *
-     * @param givenAmount The amount of either the input token or output token to swap.
+     * @param givenAmount The amount of the input token to swap.
      * @param data Data that holds information necessary to perform the swap.
-     * @return calculatedAmount The amount of either the input token or output token
-     * swapped, depending on the givenAmount inputted.
+     * @return calculatedAmount The amount of the output token swapped, depending on
+     * the givenAmount inputted.
      */
     function swap(uint256 givenAmount, bytes calldata data)
         external
@@ -33,5 +27,5 @@ interface IExecutor {
 
 interface IExecutorErrors {
     error InvalidParameterLength(uint256);
-    error UnknownCurveType(uint8);
+    error UnknownPoolType(uint8);
 }
