95d45975adf9f4b35c3746d4281cb08cdbe0f04d
royvardhan <royvardhan.official@gmail.com>
2025-01-30 19:14:22 +0530

    chore: encoder test naming

--- src/encoding/evm/strategy_encoder/encoder.rs
+++ src/encoding/evm/strategy_encoder/encoder.rs
@@ -90,7 +90,7 @@ mod tests {
     use crate::encoding::models::Swap;
 
     #[test]
-    fn test_executor_encoder() {
+    fn test_executor_strategy_encode() {
         let encoder = ExecutorStrategyEncoder {};
 
         let token_in = Bytes::from("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
