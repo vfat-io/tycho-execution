1dad4afb6b91cf86ea000afeaeba8882af73b713
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-03-18 01:37:14 -0400

    feat: Decode single and sequential swaps in LibSwap
    - The old way was useful when we just had split swaps. Unfortunately, we now have split, sequential, and single swaps, which don't always require token indices or split percentages, so we need to decode differently for each case.

--- foundry/test/LibSwap.t.sol
+++ foundry/test/LibSwap.t.sol
@@ -7,7 +7,45 @@ import "../lib/LibSwap.sol";
 contract LibSwapTest is Test {
     using LibSwap for bytes;
 
-    function testSwap() public view {
+    function testSingleSwap() public view {
+        address executor = 0x1234567890123456789012345678901234567890;
+        bytes memory protocolData = abi.encodePacked(uint256(123));
+
+        bytes memory swap = abi.encodePacked(executor, protocolData);
+        this.assertSingleSwap(swap, executor, protocolData);
+    }
+
+    function assertSingleSwap(
+        bytes calldata swap,
+        address executor,
+        bytes calldata protocolData
+    ) public pure {
+        (address decodedExecutor, bytes memory decodedProtocolData) =
+            swap.decodeSingleSwap();
+        assertEq(decodedExecutor, executor);
+        assertEq(decodedProtocolData, protocolData);
+    }
+
+    function testSequentialSwap() public view {
+        address executor = 0x1234567890123456789012345678901234567890;
+        bytes memory protocolData = abi.encodePacked(uint256(234));
+
+        bytes memory swap = abi.encodePacked(executor, protocolData);
+        this.assertSequentialSwap(swap, executor, protocolData);
+    }
+
+    function assertSequentialSwap(
+        bytes calldata swap,
+        address executor,
+        bytes calldata protocolData
+    ) public pure {
+        (address decodedExecutor, bytes memory decodedProtocolData) =
+            swap.decodeSequentialSwap();
+        assertEq(decodedExecutor, executor);
+        assertEq(decodedProtocolData, protocolData);
+    }
+
+    function testSplitSwap() public view {
         uint8 tokenInIndex = 1;
         uint8 tokenOutIndex = 2;
         uint24 split = 3;
@@ -17,20 +55,32 @@ contract LibSwapTest is Test {
         bytes memory swap = abi.encodePacked(
             tokenInIndex, tokenOutIndex, split, executor, protocolData
         );
-        this.assertSwap(swap, tokenInIndex, tokenOutIndex, split, executor);
+        this.assertSplitSwap(
+            swap, tokenInIndex, tokenOutIndex, split, executor, protocolData
+        );
     }
 
-    // This is necessary so that the compiler accepts bytes as a LibSwap.sol
-    function assertSwap(
+    // This is necessary so that the compiler accepts bytes as a LibSwap.sol for testing
+    // This is because this function takes calldata as input
+    function assertSplitSwap(
         bytes calldata swap,
         uint8 tokenInIndex,
         uint8 tokenOutIndex,
         uint24 split,
-        address executor
+        address executor,
+        bytes calldata protocolData
     ) public pure {
-        assert(swap.tokenInIndex() == tokenInIndex);
-        assert(swap.tokenOutIndex() == tokenOutIndex);
-        assert(swap.splitPercentage() == split);
-        assert(swap.executor() == executor);
+        (
+            uint8 decodedTokenInIndex,
+            uint8 decodedTokenOutIndex,
+            uint24 decodedSplit,
+            address decodedExecutor,
+            bytes memory decodedProtocolData
+        ) = swap.decodeSplitSwap();
+        assertEq(decodedTokenInIndex, tokenInIndex);
+        assertEq(decodedTokenOutIndex, tokenOutIndex);
+        assertEq(decodedSplit, split);
+        assertEq(decodedExecutor, executor);
+        assertEq(decodedProtocolData, protocolData);
     }
 }
