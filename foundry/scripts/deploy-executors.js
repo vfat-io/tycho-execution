39bd9df4b6ca8db78bb4d6757c93b30cce29f360
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-03-19 23:13:18 -0400

    fix: proper exchange name when deploying executors
    - Also update base executors in json

--- foundry/scripts/deploy-executors.js
+++ foundry/scripts/deploy-executors.js
@@ -5,32 +5,32 @@ const hre = require("hardhat");
 // Comment out the executors you don't want to deploy
 const executors_to_deploy = {
   "ethereum":[
-    // Args: Factory, Pool Init Code Hash
+    // USV2 - Args: Factory, Pool Init Code Hash
     {exchange: "UniswapV2Executor", args: [
       "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
       "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f"
       ]},
-    // Args: Factory, Pool Init Code Hash
-    {exchange: "SushiSwapV2Executor", args: [
+    // SUSHISWAP - Args: Factory, Pool Init Code Hash
+    {exchange: "UniswapV2Executor", args: [
       "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac",
       "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"
       ]},
-    // Args: Factory, Pool Init Code Hash
-    {exchange: "PancakeSwapV2Executor", args: [
+    // PANCAKESWAP V2 - Args: Factory, Pool Init Code Hash
+    {exchange: "UniswapV2Executor", args: [
       "0x1097053Fd2ea711dad45caCcc45EfF7548fCB362",
       "0x57224589c67f3f30a6b0d7a1b54cf3153ab84563bc609ef41dfb34f8b2974d2d"
       ]},
-    // Args: Factory, Pool Init Code Hash
+    // USV3 -Args: Factory, Pool Init Code Hash
     {exchange: "UniswapV3Executor", args: [
       "0x1F98431c8aD98523631AE4a59f267346ea31F984",
       "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54"
     ]},
-    // Args: Deployer, Pool Init Code Hash
-    {exchange: "PancakeSwapV3Executor", args: [
+    // PANCAKESWAP V3 - Args: Deployer, Pool Init Code Hash
+    {exchange: "UniswapV3Executor", args: [
       "0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9 ",
       "0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2"
       ]},
-     // Args: Pool manager
+    // Args: Pool manager
     {exchange: "UniswapV4Executor", args: ["0x000000000004444c5dc75cB358380D2e3dE08A90"]},
     {exchange: "BalancerV2Executor", args: []},
   ],
@@ -40,24 +40,24 @@ const executors_to_deploy = {
       "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
       "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f"
       ]},
-    // Args: Factory, Pool Init Code Hash
-    {exchange: "SushiSwapV2Executor", args: [
+    // SUSHISWAP V2 - Args: Factory, Pool Init Code Hash
+    {exchange: "UniswapV2Executor", args: [
       "0x71524B4f93c58fcbF659783284E38825f0622859",
       "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"
       ]},
-    // Args: Factory, Pool Init Code Hash
-    {exchange: "PancakeSwapV2Executor", args: [
+    // PANCAKESWAP V2 - Args: Factory, Pool Init Code Hash
+    {exchange: "UniswapV2Executor", args: [
       "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",
       "0x57224589c67f3f30a6b0d7a1b54cf3153ab84563bc609ef41dfb34f8b2974d2d"
       ]},
-    // Args: Factory, Pool Init Code Hash
+    // USV3 - Args: Factory, Pool Init Code Hash
     {exchange: "UniswapV3Executor", args: [
       "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
       "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54"
     ]},
-    // Args: Deployer, Pool Init Code Hash
-    {exchange: "PancakeSwapV3Executor", args: [
-      "0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9 ",
+    // PANCAKESWAP V3 - Args: Deployer, Pool Init Code Hash
+    {exchange: "UniswapV3Executor", args: [
+      "0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9",
       "0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2"
       ]},
     // Args: Pool manager
