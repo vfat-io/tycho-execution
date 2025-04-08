ed90273d7e4c821366bba27a9707acb89e6d8b0a
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-04-08 11:31:35 -0400

    refactor: create Permit2TestHelper
    - To avoid duplicating permit2 setup code for TychoRouter and executor tests.

--- foundry/test/executors/UniswapV2Executor.t.sol
+++ foundry/test/executors/UniswapV2Executor.t.sol
@@ -5,6 +5,7 @@ import "@src/executors/UniswapV2Executor.sol";
 import "@src/executors/ExecutorTransferMethods.sol";
 import {Test} from "../../lib/forge-std/src/Test.sol";
 import {Constants} from "../Constants.sol";
+import {Permit2TestHelper} from "../Permit2TestHelper.sol";
 
 contract UniswapV2ExecutorExposed is UniswapV2Executor {
     constructor(address _factory, bytes32 _initCode, address _permit2)
@@ -48,7 +49,7 @@ contract FakeUniswapV2Pool {
     }
 }
 
-contract UniswapV2ExecutorTest is Test, Constants {
+contract UniswapV2ExecutorTest is Test, Constants, Permit2TestHelper {
     using SafeERC20 for IERC20;
 
     UniswapV2ExecutorExposed uniswapV2Exposed;
@@ -188,86 +189,6 @@ contract UniswapV2ExecutorTest is Test, Constants {
         assertGe(finalBalance, amountOut);
     }
 
-    // TODO generalize these next two methods - don't reuse from TychoRouterTestSetup
-    /**
-     * @dev Handles the Permit2 approval process for Alice, allowing the TychoRouter contract
-     *      to spend `amount_in` of `tokenIn` on her behalf.
-     *
-     * This function approves the Permit2 contract to transfer the specified token amount
-     * and constructs a `PermitSingle` struct for the approval. It also generates a valid
-     * EIP-712 signature for the approval using Alice's private key.
-     *
-     * @param tokenIn The address of the token being approved.
-     * @param amount_in The amount of tokens to approve for transfer.
-     * @return permitSingle The `PermitSingle` struct containing the approval details.
-     * @return signature The EIP-712 signature for the approval.
-     */
-    function handlePermit2Approval(address tokenIn, uint256 amount_in)
-        internal
-        returns (IAllowanceTransfer.PermitSingle memory, bytes memory)
-    {
-        IERC20(tokenIn).approve(PERMIT2_ADDRESS, amount_in);
-        IAllowanceTransfer.PermitSingle memory permitSingle = IAllowanceTransfer
-            .PermitSingle({
-            details: IAllowanceTransfer.PermitDetails({
-                token: tokenIn,
-                amount: uint160(amount_in),
-                expiration: uint48(block.timestamp + 1 days),
-                nonce: 0
-            }),
-            spender: address(uniswapV2Exposed),
-            sigDeadline: block.timestamp + 1 days
-        });
-
-        bytes memory signature = signPermit2(permitSingle, ALICE_PK);
-        return (permitSingle, signature);
-    }
-
-        /**
-     * @dev Signs a Permit2 `PermitSingle` struct with the given private key.
-     * @param permit The `PermitSingle` struct to sign.
-     * @param privateKey The private key of the signer.
-     * @return The signature as a `bytes` array.
-     */
-    function signPermit2(
-        IAllowanceTransfer.PermitSingle memory permit,
-        uint256 privateKey
-    ) internal view returns (bytes memory) {
-        bytes32 _PERMIT_DETAILS_TYPEHASH = keccak256(
-            "PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)"
-        );
-        bytes32 _PERMIT_SINGLE_TYPEHASH = keccak256(
-            "PermitSingle(PermitDetails details,address spender,uint256 sigDeadline)PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)"
-        );
-        bytes32 domainSeparator = keccak256(
-            abi.encode(
-                keccak256(
-                    "EIP712Domain(string name,uint256 chainId,address verifyingContract)"
-                ),
-                keccak256("Permit2"),
-                block.chainid,
-                PERMIT2_ADDRESS
-            )
-        );
-        bytes32 detailsHash =
-            keccak256(abi.encode(_PERMIT_DETAILS_TYPEHASH, permit.details));
-        bytes32 permitHash = keccak256(
-            abi.encode(
-                _PERMIT_SINGLE_TYPEHASH,
-                detailsHash,
-                permit.spender,
-                permit.sigDeadline
-            )
-        );
-
-        bytes32 digest =
-            keccak256(abi.encodePacked("\x19\x01", domainSeparator, permitHash));
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
-
-        return abi.encodePacked(r, s, v);
-    }
-
-
     function testSwapWithPermit2TransferFrom() public {
         uint256 amountIn = 10 ** 18;
         uint256 amountOut = 1847751195973566072891;
@@ -280,13 +201,14 @@ contract UniswapV2ExecutorTest is Test, Constants {
             uint8(ExecutorTransferMethods.TransferMethod.TRANSFERPERMIT2)
         );
 
-
         deal(WETH_ADDR, ALICE, amountIn);
         vm.startPrank(ALICE);
         (
             IAllowanceTransfer.PermitSingle memory permitSingle,
             bytes memory signature
-        ) = handlePermit2Approval(WETH_ADDR, amountIn);
+        ) = handlePermit2Approval(
+            WETH_ADDR, address(uniswapV2Exposed), amountIn
+        );
 
         // Assume the permit2.approve method will be called from the TychoRouter
         // Replicate this secnario in this test.
